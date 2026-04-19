6fd84d4efb1d0b743c994b0d4233845677fb644d
philipestevenson-RumHam <philipestevenson@gmail.com>
2026-04-19 01:55:52 +0100

    c
    c

--- old - build-images.bat
+++ old - build-images.bat
@@ -8,22 +8,29 @@ echo {> images.json
 
 for /D %%D in (products\*) do (
     set "folder=%%~nxD"
-    set "imgList="
+    set "imgFirst=1"
     set "hasImages=0"
 
     for %%F in ("%%D\*.jpg" "%%D\*.jpeg" "%%D\*.png" "%%D\*.gif" "%%D\*.webp" "%%D\*.avif") do (
         set "hasImages=1"
-        if "!imgList!"=="" (
-            set "imgList="%%~nxF""
-        ) else (
-            set "imgList=!imgList!, "%%~nxF""
-        )
     )
 
     if !hasImages!==1 (
         if !first!==0 echo ,>> images.json
         set "first=0"
-        echo   "!folder!": [!imgList!]>> images.json
+
+        set /p ="  "!folder!": ["< nul >> images.json
+
+        for %%F in ("%%D\*.jpg" "%%D\*.jpeg" "%%D\*.png" "%%D\*.gif" "%%D\*.webp" "%%D\*.avif") do (
+            if !imgFirst!==1 (
+                set "imgFirst=0"
+                set /p ="""%%~nxF"""< nul >> images.json
+            ) else (
+                set /p =", "%%~nxF""< nul >> images.json
+            )
+        )
+
+        echo ]>> images.json
     )
 )
 
